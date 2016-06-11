5ef1b0f84f8c6e438e5c47bd0b38d9614d3fee4d
wds2006sdo <641614152@qq.com>
2016-06-11 13:40:30 +0800

    add 15000 comments

--- Filter/convert2json.py
+++ Filter/convert2json.py
@@ -24,8 +24,8 @@ def write_file(filepath,json_data):
 
 
 if __name__ == '__main__':
-    filepath = 'comment_5000.json'
-    sql = 'select cid,content from comment_withuser limit 4000,1000'
+    filepath = 'comment_20000.json'
+    sql = 'select cid,content from comment_withuser limit 19000,1000'
 
     json_list = read_db(sql)
     json_data = json.dumps(json_list)
